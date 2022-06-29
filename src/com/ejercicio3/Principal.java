b8caaf29b0c69b0ff276171f56ca44a878110d9c
jamuru <jamuru@gmail.com>
2022-06-29 17:48:18 -0300

    Ejercicio 3 corregido (se corrige logica y se agregan break's en el switch, se agregan try/catch de inputs)

--- src/com/ejercicio3/Principal.java
+++ src/com/ejercicio3/Principal.java
@@ -1,6 +1,7 @@
 package com.ejercicio3;
 
 import java.text.DecimalFormat;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -9,44 +10,57 @@ public class Principal {
         double sueldo = 0;
         byte area = 0;
         byte cantidadHijos = 0;
-        double bonificacion = 0.0;
+        double bonificaArea = 0.0;
+        double bonificaHijos = 0.0;
         double sueldoLikido = 0.0;
-
-        DecimalFormat decimales = new DecimalFormat("#.00");
 
         System.out.println("Sofka U :: Training - Intro JAVA - Mentoria 29 de junio");
         System.out.println("Ejercicio 3 :: Jorge MARTINEZ");
         System.out.println("-------------------------------------------");
         System.out.println(">>>>>>>>>>>>>>>> Calcular sueldo <<<<<<<<<<<<<<<<<<<<");
 
-        System.out.println("Ingrese el sueldo nominal:");
-        sueldo = lector.nextDouble();
-        System.out.println("Ingrese el Area de Trabajo (1: SISTEMAS, 2: CONTABILIDAD, 0: OTROS)");
-        area = lector.nextByte();
-        System.out.println("Ingrese la cantidad de hijos:");
-        cantidadHijos = lector.nextByte();
+        try{
+            System.out.println("Ingrese el sueldo nominal:");
+            sueldo = lector.nextDouble();
+        }catch (InputMismatchException e){
+            System.out.println("¡¡¡ERROR--DEBE INGRESAR UN NUMERO!!!");
+        }
+        try{
+            System.out.println("Ingrese el Area de Trabajo (1: SISTEMAS, 2: CONTABILIDAD, 0: OTROS)");
+            area = lector.nextByte();
+        }catch (InputMismatchException e){
+            System.out.println("¡¡¡ERROR--DEBE INGRESAR UN NUMERO!!!");
+        }
+        try{
+            System.out.println("Ingrese la cantidad de hijos:");
+            cantidadHijos = lector.nextByte();
+        }catch (InputMismatchException e){
+            System.out.println("¡¡¡ERROR--DEBE INGRESAR UN NUMERO!!!");
+        }
 
         //Calculo la bonificacion por el area de trabajo
         switch(area){
             case 1:
-                bonificacion = (sueldo * 25) / 100;
+                bonificaArea = (sueldo * 25) / 100;
+                break;
             case 2:
-                bonificacion = (sueldo * 20) / 100;
+                bonificaArea = (sueldo * 20) / 100;
+                break;
             default:
-                bonificacion = (sueldo * 10) / 100;
+                bonificaArea = (sueldo * 10) / 100;
         }
 
         //Calculo la bonicificacion para cantidad de hijos >= 2
         if (cantidadHijos >=2){
-            bonificacion = bonificacion + (cantidadHijos * 150);
+            bonificaHijos = cantidadHijos * 150;
         }
 
         //Calculo el sueldo likido
-        sueldoLikido = sueldo + bonificacion;
+        sueldoLikido = sueldo + bonificaArea + bonificaHijos;
 
         System.out.println("______________________________________________________");
-        System.out.println("Su bonificacion por area es de : " + bonificacion);
-        System.out.println("Su bonificacion por cantidad de hijos es de: " + (cantidadHijos*150));
+        System.out.println("Su bonificacion por area es de : " + bonificaArea);
+        System.out.println("Su bonificacion por cantidad de hijos es de: " + bonificaHijos);
         System.out.println("______________________________________________________");
         System.out.println("El Sueldo TOTAL es de: " + sueldoLikido);
 
